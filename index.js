2fefc9c4609de3d955dffb4c1f613a3725d932ab
Filip Madunicky <yelidric@gmail.com>
2025-10-04 22:05:08 +0200

    Serves static files from the dist directory
    Removes the cors middleware and instead configures the server to serve static files from the "dist" directory.
    
    This allows the frontend application (presumably built and placed in the "dist" directory) to be served directly by the backend, simplifying deployment and removing the need for separate CORS configuration.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,7 +7,7 @@ morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
 
-app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
