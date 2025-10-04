580d092bcaf0553cf7509395cd01e612bcacc951
Filip Madunicky <yelidric@gmail.com>
2025-10-04 20:52:13 +0200

    Enables CORS for the application
    Adds CORS middleware to the application to allow cross-origin requests.
    Also sets the PORT environment variable to be used in deployment.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
